69ae11fb8418ba704375980e85d01a7998097d51
KHS <ewoking777@gmail.com>
2024-02-01 12:46:09 +0900

    섹션 3 : 영속성 관리 - 내부 동작방식 | 영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,31 +17,37 @@ public class JpaMain {
         try {
             //저장
             //비영속
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("helloA");
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
 
             //영속 -> 바로 DB에 쿼리 나가지 않음. 영속성 컨텍스트에 저장이 되는 것
 //            em.persist(member);
 
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            System.out.println("=== AFTER ===");
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+
+            System.out.println("====================");
 
             //조회
-//            Member findMember = em.find(Member.class, 2L);
-//            System.out.println("findMember = " + findMember.getId());
-//            System.out.println("findMember = " + findMember.getName());
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+
+//            System.out.println("result = " + (findMember1 == findMember2));
 
             //수정
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("HelloJPA");
 
             //전체조회
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
 
 //            for (Member member : result) {
 //                System.out.println("member.name = " + member.getName());

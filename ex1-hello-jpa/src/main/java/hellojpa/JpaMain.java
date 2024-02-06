bc33d4e7f78c0d107cbf0a3b55e1d5eca943a121
khs <ewoking777@gmail.com>
2024-02-06 23:00:58 +0900

    섹션 5 : 연관관계 매핑 기초 | 양방향 연관관계와 연관관계의 주인 1- 기본

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -30,8 +30,11 @@ public class JpaMain {
             em.clear();
 
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
 
 
             tx.commit();
